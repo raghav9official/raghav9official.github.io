4d97e222c69ef6e96dcf23cd01511040252931b3
Raghav Yadav <79745774+raghav9official@users.noreply.github.com>
2021-05-25 12:50:06 +0530

    added offline support

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ new Vue({
         icon: './icon.png'
     }
 });
+
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js');
+}
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('👍', 'beforeinstallprompt', event);
+    window.deferredPrompt = event;
+    document.querySelector("#install").classList.toggle('hidden', false);
+});
+
+document.querySelector("#install").addEventListener('click', async () => {
+    console.log('👍', 'butInstall-clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    window.deferredPrompt = null;
+    divInstall.classList.toggle('hidden', true);
+});
